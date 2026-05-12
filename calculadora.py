1b5b38ee41e2ef17ff8c28dde4eed954ece8185d
Túlio Menezes Bessa <dev.tuliobessa@gmail.com>
2026-05-12 20:35:24 -0300

    potencia
    add potencia ao codigo

--- calculadora.py
+++ calculadora.py
@@ -1,16 +1,20 @@
+import math
+
 class Calculadora:
     
     def soma(self, a, b):
-        return round (a + b, 2)
+        return round(a + b, 2)
     
-    def subtrai(self, a,b):
+    def subtrai(self, a, b):
         return a - b
     
-    def multiplica(self, a,b):
+    def multiplica(self, a, b):
         return a * b
     
-    def divide(self, a,b):
+    def divide(self, a, b):
         if b == 0:
             raise ValueError("Não é permitida divisão por zero")
         return a / b
     
+    def potencia(self, a, b):
+        return a ** b
